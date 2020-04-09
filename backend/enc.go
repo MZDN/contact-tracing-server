62c0ebf83e3ac9d3333564dd668913531e47d512
MK <17832330+mkchungs@users.noreply.github.com>
2020-04-08 17:21:02 -0700

    memo protoBuf & Sign/VerifySign (#6)

--- backend/enc.go
+++ backend/enc.go
@@ -1,49 +1,193 @@
 package backend
 
 import (
-	crypto_rand "crypto/rand"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/asn1"
 	"fmt"
 	"io"
+	"log"
+	"math/big"
 
-	"golang.org/x/crypto/nacl/box"
+	"github.com/gogo/protobuf/proto"
 )
 
-type CENReportV4 struct {
-	HashedPK   []byte `json:"hashedPK"`
-	EncodedMsg []byte `json:"encodedMsg"` // enc( “1”, PK_A)
-	// protobuf of ints symptoms, diseases, signature
-	// just to prevent attack, verified against EncMsg
-	// NO user timestamp
+const (
+	PrefixSize      = 3
+	PublicKeyPrefix = 0
+	RawSigPrefix    = 1
+	MemoPrefix      = 2
+)
+
+// ECDSASignature storing R,S for DER?
+type ECDSASignature struct {
+	R, S *big.Int
 }
 
-func Encrypt(symptoms []byte, recipientPublicKey *[32]byte, senderPrivateKey *[32]byte) (encrypted []byte, err error) {
-	var nonce [24]byte
-	if _, err := io.ReadFull(crypto_rand.Reader, nonce[:]); err != nil {
+// P256 encapsulates the ECDSA P256 curve
+func P256() elliptic.Curve {
+	return elliptic.P256()
+}
+
+// FromECDSAPub maps a ecdsa PublicKey into raw byte
+func FromECDSAPub(pub *ecdsa.PublicKey) []byte {
+	if pub == nil || pub.X == nil || pub.Y == nil {
+		return nil
+	}
+	return elliptic.Marshal(P256(), pub.X, pub.Y)
+}
+
+// ByteToPublicKey maps a public key in byte form into a public key
+func ByteToPublicKey(pubByte []byte) (*ecdsa.PublicKey, error) {
+	x, y := elliptic.Unmarshal(P256(), pubByte)
+	if x == nil {
+		return nil, fmt.Errorf("Invalid pubkey")
+	}
+	pub := ecdsa.PublicKey{Curve: P256(), X: x, Y: y}
+	return &pub, nil
+}
+
+// Sign uses the private key to sign a memo m
+func Sign(priv *ecdsa.PrivateKey, m []byte) ([]byte, error) {
+	r, s, err := ecdsa.Sign(rand.Reader, priv, m)
+	if err != nil {
 		return nil, err
 	}
-	encrypted = box.Seal(nonce[:], symptoms, &nonce, recipientPublicKey, senderPrivateKey)
-	return encrypted, nil
-}
-
-func Decrypt(encrypted []byte, senderPublicKey *[32]byte, recipientPrivateKey *[32]byte) (decrypted []byte, err error) {
-	var decryptNonce [24]byte
-	copy(decryptNonce[:], encrypted[:24])
-	decrypted, ok := box.Open(nil, encrypted[24:], &decryptNonce, senderPublicKey, recipientPrivateKey)
-	if !ok {
-		return nil, fmt.Errorf("decryption error")
+	signed, err := asn1.Marshal(ECDSASignature{r, s})
+	if err != nil {
+		return nil, err
 	}
-	fmt.Println(string(decrypted))
-	return decrypted, nil
+
+	pubkey := FromECDSAPub(&priv.PublicKey) // this is uncompressed 65 bytes key
+	prefix := make([]byte, PrefixSize)
+	prefix[PublicKeyPrefix] = byte(len(pubkey))
+	prefix[RawSigPrefix] = byte(len(signed))
+	prefix[MemoPrefix] = byte(len(m))                            // prefix[len(pkey),len(sig),len(m)]
+	sigWithPubkeyMemo := append(append(pubkey, signed...), m...) // [PK, sig, memo]
+	fullsig := append(prefix, sigWithPubkeyMemo...)              // [prefix,PK, sig, memo]
+	fmt.Printf("\n[pk=%d,sig=%d,m=%d]\npk=%x\nsig=%x\nm=%x\n", len(pubkey), len(signed), len(m), pubkey, signed, m)
+	return fullsig, nil
 }
 
-func MakeCENReport(symptoms []byte, recipientPublicKey *[32]byte, senderPrivateKey *[32]byte) (report CENReport, err error) {
-	encrypted, err := Encrypt(symptoms, recipientPublicKey, senderPrivateKey)
+// VerifySign uses the signature and a message m to verify against a public key
+func VerifySign(signature []byte) ([]byte, error) {
+	prefix := signature[:PrefixSize]
+	pubKeySize := prefix[PublicKeyPrefix]
+	rawSigSize := prefix[RawSigPrefix]
+
+	sigWithPubkeyMemo := signature[PrefixSize:]
+
+	pubByte := sigWithPubkeyMemo[:pubKeySize] // PK of [PK, sig, memo]
+	pub, err := ByteToPublicKey(pubByte)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Printf("\nPublic key %x (%x %x)\n", FromECDSAPub(pub), pub.X, pub.Y)
+
+	signed := sigWithPubkeyMemo[pubKeySize : pubKeySize+rawSigSize] // sig of [PK, sig, memo]
+	fmt.Printf("\nrawsig=%x ,len(rawsig)=%d\n", signed, len(signed))
+	rawsig := &ECDSASignature{}
+	_, err = asn1.Unmarshal(signed, rawsig)
+	if err != nil {
+		return nil, err
+	}
+
+	m := sigWithPubkeyMemo[pubKeySize+rawSigSize:] // m of [PK, sig, memo]
+	if ok := ecdsa.Verify(pub, m, rawsig.R, rawsig.S); !ok {
+		return nil, fmt.Errorf("signature invalid")
+	}
+	return m, nil
+}
+
+// Encrypt encodes a plaintext msg using session secret and returns ciphertext after
+func Encrypt(ss []byte, msg []byte) ([]byte, error) {
+	nonce := make([]byte, 12)
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		panic(err.Error())
+	}
+	block, err := aes.NewCipher(ss)
+	if err != nil {
+		return nil, err
+	}
+
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	cipherText := aesgcm.Seal(nil, nonce, msg, nil)
+	cipherText = append(nonce, cipherText...)
+	return cipherText, nil
+}
+
+// Decrypt decode a cipherText using session secret and returns plaintext msg
+func Decrypt(ss []byte, cipherText []byte) ([]byte, error) {
+	decryptNonce := make([]byte, 12)
+	copy(decryptNonce[:], cipherText[:12])
+	block, err := aes.NewCipher(ss)
+	if err != nil {
+		return nil, err
+	}
+
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	plainText, err := aesgcm.Open(nil, decryptNonce, cipherText[12:], nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return plainText, nil
+}
+
+// MakeCENReport generate the cenReport using rPub, sPriv, memoByte
+func MakeCENReport(rPub *ecdsa.PublicKey, sPriv *ecdsa.PrivateKey, memoByte []byte) (report CENReport, err error) {
+	ss := GenerateSessionSecret(rPub, sPriv)
+	encrypted, err := Encrypt(ss[:], memoByte)
 	if err != nil {
 		return report, err
 	}
-	var rPub []byte
-	copy(rPub[:], recipientPublicKey[:32])
-	hashedPK := Computehash(rPub)
+	hashedPK := Computehash(FromECDSAPub(rPub))
 	report = CENReport{HashedPK: hashedPK, EncodedMsg: encrypted}
 	return report, nil
+}
+
+// GenerateSessionSecret computes session secret. Result should be stored as tuple map[hashedPK] => (pub_K, ss)
+func GenerateSessionSecret(rPub *ecdsa.PublicKey, sPriv *ecdsa.PrivateKey) [32]byte {
+	a, _ := rPub.Curve.ScalarMult(rPub.X, rPub.Y, sPriv.D.Bytes())
+	ss := sha256.Sum256(a.Bytes())
+	return ss
+}
+
+// DecryptCENReport decrypts CENReport using session secret. Should be invoked only if hashedPK we found
+func DecryptCENReport(report CENReport, ss []byte) (memoByte []byte, err error) {
+	cipherText := report.EncodedMsg
+	if memoByte, err = Decrypt(ss, cipherText); err != nil {
+		return memoByte, err
+	}
+	return memoByte, err
+}
+
+func (m *Memo) Bytes() (mbyte []byte) {
+	mByte, err := proto.Marshal(m)
+	if err != nil {
+		log.Fatal("marshaling error: ", err)
+		return nil
+	}
+	return mByte
+}
+
+func FromMemoByte(mbyte []byte) (m *Memo) {
+	m = &Memo{}
+	err := proto.Unmarshal(mbyte, m)
+	if err != nil {
+		log.Fatal("unmarshaling error: ", err)
+	}
+	return m
 }
--- backend/enc_test.go
+++ backend/enc_test.go
@@ -1,38 +1,44 @@
 package backend
 
 import (
-	crypto_rand "crypto/rand"
+	"bytes"
+	"crypto/ecdsa"
+	"crypto/elliptic"
 	"encoding/json"
 	"fmt"
-	"io"
+	"log"
 	"testing"
 
-	"golang.org/x/crypto/nacl/box"
-
-	"crypto/aes"
-	"crypto/cipher"
 	"crypto/rand"
+
+	"github.com/golang/protobuf/proto"
 )
 
 func TestEncryption(t *testing.T) {
-	senderPublicKey, senderPrivateKey, err := box.GenerateKey(crypto_rand.Reader)
-	if err != nil {
-		panic(err)
-	}
+	rPriv, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	sPriv, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	rPub := &rPriv.PublicKey
+	sPub := &sPriv.PublicKey
 
-	recipientPublicKey, recipientPrivateKey, err := box.GenerateKey(crypto_rand.Reader)
-	if err != nil {
-		panic(err)
+	fmt.Printf("\nPrivate key (sender) %x", sPriv.D)
+	fmt.Printf("\nPublic key (sender) (%x %x)\n", sPub.X, sPub.Y)
+	fmt.Printf("\nPublic key (recipient) (%x,%x)\n", rPub.X, rPub.Y)
+
+	sss := GenerateSessionSecret(rPub, sPriv) //sender generated ss
+	rss := GenerateSessionSecret(sPub, rPriv) //recipient generated ss
+	fmt.Printf("\nsession secret: [sss=%x] [rss=%x] \n", sss, rss)
+	if !bytes.Equal(sss[:], rss[:]) {
+		panic("session secret mismatch!")
 	}
 
 	msg := []byte("severe fever,coughing,hard to breathe")
-	encrypted, err := Encrypt(msg, recipientPublicKey, senderPrivateKey)
+	encrypted, err := Encrypt(sss[:], msg)
 	if err != nil {
 		t.Fatalf("err:%v", err)
 	}
-	fmt.Printf("Encrypted:%x len(RawMsg)=%d, len(EncMsg)=%d\n", encrypted, len(msg), len(encrypted))
+	fmt.Printf("\nEncrypted:%x len(RawMsg)=%d, len(EncMsg)=%d\n", encrypted, len(msg), len(encrypted))
 
-	decrypted, err := Decrypt(encrypted, senderPublicKey, recipientPrivateKey)
+	decrypted, err := Decrypt(rss[:], encrypted)
 	if err != nil {
 		panic("decryption error")
 	}
@@ -40,18 +46,73 @@ func TestEncryption(t *testing.T) {
 }
 
 func TestCENReport(t *testing.T) {
-	_, senderPrivateKey, err := box.GenerateKey(crypto_rand.Reader)
+
+	fmt.Printf("--ECC Parameters--\n")
+	fmt.Printf(" Name: %s\n", elliptic.P256().Params().Name)
+	fmt.Printf(" N: %x\n", elliptic.P256().Params().N)
+	fmt.Printf(" P: %x\n", elliptic.P256().Params().P)
+	fmt.Printf(" Gx: %x\n", elliptic.P256().Params().Gx)
+	fmt.Printf(" Gy: %x\n", elliptic.P256().Params().Gy)
+	fmt.Printf(" Bitsize: %x\n\n", elliptic.P256().Params().BitSize)
+
+	rPriv, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	sPriv, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	rPub := &rPriv.PublicKey
+	sPub := &sPriv.PublicKey
+
+	fmt.Printf("\nPrivate key (sender) %x", sPriv.D)
+	fmt.Printf("\nPublic key (sender) %x (%x %x)\n", FromECDSAPub(sPub), sPub.X, sPub.Y)
+	fmt.Printf("\nPublic key (recipient) %x (%x,%x)\n", FromECDSAPub(rPub), rPub.X, rPub.Y)
+
+	sss := GenerateSessionSecret(rPub, sPriv) //sender generated ss
+	rss := GenerateSessionSecret(sPub, rPriv) //recipient generated ss
+	fmt.Printf("\nsession secret: [sss=%x] [rss=%x] \n", sss, rss)
+	if !bytes.Equal(sss[:], rss[:]) {
+		panic("session secret mismatch!")
+	}
+
+	//memo := []byte("high fever, dry cough,hard to breathe")
+	//memo := &Memo{ReportType: 1, DiseaseID: 2, SymptomID: []int32{5, 7, 123}}
+
+	//Step 1.a - A sends these bytes: [PK_A(t), siga, memoA] to B
+	memoA := &Memo{ReportType: 0, DiseaseID: 0, SymptomID: []int32{7}}
+	memoByteA := memoA.Bytes()
+	sigA, err := Sign(sPriv, memoByteA)
 	if err != nil {
 		panic(err)
 	}
-
-	recipientPublicKey, _, err := box.GenerateKey(crypto_rand.Reader)
+	fmt.Printf("\nSigned: memoByteA=[%x]\nsigA=%x\n", memoByteA, sigA)
+	vmemoByteA, err := VerifySign(sigA)
 	if err != nil {
 		panic(err)
 	}
+	fmt.Printf("\nVerified: vmemoByteA=[%x]\n", vmemoByteA)
+	vMemoA := FromMemoByte(vmemoByteA)
+	fmt.Printf("\nDecrypted Msg: {reportType=%v, vmemoByteA=[%s]} (msg=[%x])\n", vMemoA.GetReportType(), vMemoA.String(), vmemoByteA)
 
-	symptoms := []byte("high fever, dry cough,hard to breathe")
-	report, err := MakeCENReport(symptoms, recipientPublicKey, senderPrivateKey)
+	//Step 1.b - B sends these bytes: [PK_B(t), sigb, memoB] to A
+	memoB := &Memo{ReportType: 0, DiseaseID: 0}
+	memoByteB := memoB.Bytes()
+	sigB, err := Sign(sPriv, memoByteB)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("\nSigned: memoByteB=[%x]\nsigB=%x\n", memoByteB, sigB)
+	vmemoByteB, err := VerifySign(sigB)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("\nVerified: vmemoByteB=[%x]\n", vmemoByteB)
+	vMemoB := FromMemoByte(vmemoByteB)
+	fmt.Printf("\nDecrypted Msg: {reportType=%v, vmemoByteB=[%s]} (msg=[%x])\n", vMemoB.GetReportType(), vMemoB.String(), vmemoByteB)
+
+	//Step2 - Encryption. A is sick and computes EncodedMsg following a protobuf serialization scheme within a CENReport R using (1b)'s PK_B(t)
+	memoS := &Memo{ReportType: 1, DiseaseID: 2, SymptomID: []int32{5, 7, 123}}
+	memoByteS, err := proto.Marshal(memoS)
+	if err != nil {
+		log.Fatal("marshaling error: ", err)
+	}
+	report, err := MakeCENReport(rPub, sPriv, memoByteS)
 	if err != nil {
 		panic(err)
 	}
@@ -59,39 +120,26 @@ func TestCENReport(t *testing.T) {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("msg: %v\n", string(r))
-}
+	fmt.Printf("\nreport: %v\n", string(r))
 
-func TestGCM(t *testing.T) {
-	plaintext := []byte("severe fever,coughing,hard to breathe")
+	//A's report, which is included in reports []CENReport sent to ProcessReport
+	//TODO: backend.ProcessReport([]CENReport{report})
 
-	key := make([]byte, 32)
-	if _, err := io.ReadFull(rand.Reader, key); err != nil {
-		panic(err.Error())
-	}
+	//Step3.a - Decryption. B queries a report by hitting ProcessQuery(query []byte, timestamp uint64) and getting back reports []CENReport one of which contains R of Step2
+	//TODO: backend.ProcessQuery(query []byte, timestamp uint64)
 
-	block, err := aes.NewCipher(key)
+	//Step3.b - B filter query with seen map[hashedPk](pub,rss).
+	//TODO: B filter query with seen map[hashedPk](pub,rss)
+
+	//Step3.c - B Then uses recipient session secret generate by (1a)'s PK_A(t) to decode to the memo, following a protobuf deserialization scheme
+	dmemoByteS, err := DecryptCENReport(report, rss[:])
 	if err != nil {
-		panic(err.Error())
+		panic(err)
 	}
-
-	nonce := make([]byte, 12)
-	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
-		panic(err.Error())
-	}
-
-	aesgcm, err := cipher.NewGCM(block)
+	vMemoS := &Memo{}
+	err = proto.Unmarshal(dmemoByteS, vMemoS)
 	if err != nil {
-		panic(err.Error())
+		log.Fatal("unmarshaling error: ", err)
 	}
-
-	ciphertext := aesgcm.Seal(nil, nonce, plaintext, nil)
-	fmt.Printf("Key:\t\t%x", key)
-	fmt.Printf("\nCiphertext:\t%x", ciphertext)
-
-	unsealedtext, err := aesgcm.Open(nil, nonce, []byte(ciphertext), nil)
-	if err != nil {
-		panic(err.Error())
-	}
-	fmt.Printf("\nunsealedtext:\t%v", string(unsealedtext))
+	fmt.Printf("\nDecrypted Memo: {reportType=%v, vMemoS=[%s]} (msg=[%x])\n", vMemoS.GetReportType(), vMemoS.String(), dmemoByteS)
 }
