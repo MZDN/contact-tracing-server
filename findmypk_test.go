86123defc13b0b73e9e053f4630bf60167e8f6c5
mkchungs <michael@wolk.com>
2020-04-10 14:37:09 -0700

    WIP

--- findmypk_test.go
+++ findmypk_test.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"bytes"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"math/rand"
 	"net"
 	"net/http"
-	"os"
 	"testing"
 	"time"
 
@@ -16,7 +16,7 @@ import (
 	"github.com/wolkdb/findmypk-server/server"
 )
 
-const endpoint = "api.wolk.com"
+const endpoint = "fmpk.wolk.com"
 
 // DefaultTransport contains all HTTP client operation parameters
 var DefaultTransport http.RoundTripper = &http.Transport{
@@ -62,35 +62,14 @@ func httppost(url string, body []byte) (result []byte, err error) {
 	return result, nil
 }
 
-func httpget(url string) (result []byte, err error) {
-
-	httpclient := &http.Client{Timeout: time.Second * 120, Transport: DefaultTransport}
-
-	req, err := http.NewRequest(http.MethodGet, url, nil)
-	if err != nil {
-		return result, fmt.Errorf("[findmypk_test:httpget] %s", err)
-	}
-
-	resp, err := httpclient.Do(req)
-	if err != nil {
-		return result, fmt.Errorf("[findmypk_test:httpget] %s", err)
-	}
-
-	result, err = ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return result, fmt.Errorf("[findmypk_test:httpget] %s", err)
-	}
-	resp.Body.Close()
-
-	return result, nil
-}
-
 func TestFMPKSimple(t *testing.T) {
-	hostname, err := os.Hostname()
-	if err != nil {
-	}
-	localendpoint := fmt.Sprintf("%s.wolk.com:%s", hostname, server.DefaultPort)
-	localendpoint = endpoint
+	/*
+		hostname, err := os.Hostname()
+		if err != nil {
+		}
+		localendpoint := fmt.Sprintf("%s.wolk.com:%s", hostname, server.DefaultPort)
+	*/
+	localendpoint := endpoint
 	timestamp := time.Now().Unix()
 
 	var reports []backend.FMReport
@@ -105,11 +84,16 @@ func TestFMPKSimple(t *testing.T) {
 		report := backend.FMReport{HashedPK: hashKey, EncodedMsg: []byte(symptom)}
 		reports = append(reports, report)
 	}
+
+	// Post FMReports to /report
 	fmReportJSON, err := json.Marshal(reports)
-	_, err = httppost(fmt.Sprintf("https://%s/%s", localendpoint, server.EndpointFMReport), fmReportJSON)
+	fmReportURL := fmt.Sprintf("https://%s/%s", localendpoint, server.EndpointFMReport)
+	fmt.Printf("\nPOST Report:\n curl -X POST \"%v\" -d '%v'\n", fmReportURL, string(fmReportJSON))
+	res, err := httppost(fmReportURL, fmReportJSON)
 	if err != nil {
 		t.Fatalf("EndpointFMReport: %s", err)
 	}
+	fmt.Printf("\nPOST Report Result:%s\n", res)
 
 	var prefixHashedKey []byte
 	sampleKey := hashKeys[3]
@@ -125,17 +109,24 @@ func TestFMPKSimple(t *testing.T) {
 	   prefixHashedKey = append(prefixHashedKey, (sampleKey2[0]&03<<6)|(sampleKey2[1]&0xFC>>2))
 	   prefixHashedKey = append(prefixHashedKey, (sampleKey2[1]&03<<6)|(sampleKey2[2]&0xFC>>2))
 	*/
+	fmQueryUrl := fmt.Sprintf("https://%s/%s/%d", localendpoint, server.EndpointFMQuery, timestamp)
+	prefixHashedKeyByte := base64.StdEncoding.EncodeToString(prefixHashedKey)
+	fmt.Printf("\nprefixHashedKey:%v\n", prefixHashedKey)
+	//b64.URLEncoding.DecodeString(uEnc)
+	fmt.Printf("\nPOST Query:\n curl -X POST \"%v\" --data-binary '%s'\n", fmQueryUrl, prefixHashedKeyByte)
 
-	result, err := httppost(fmt.Sprintf("https://%s/%s/%d", localendpoint, server.EndpointFMQuery, timestamp), prefixHashedKey)
+	res, err = httppost(fmQueryUrl, prefixHashedKey)
 	if err != nil {
 		t.Fatalf("EndpointFMReport: %s", err)
 	}
+	fmt.Printf("\nPOST Query Result:%v\n", string(res))
 
 	var resultreport []*backend.FMReport
-	err = json.Unmarshal(result, &resultreport)
+	err = json.Unmarshal(res, &resultreport)
 	if err != nil {
 		t.Fatalf("EndpointFMReport(check1): %s", err)
 	}
+	fmt.Printf("\nPOST Query resultreport: %v\n", string(res))
 	for _, r := range resultreport {
 		if bytes.Compare(r.HashedPK, sampleKey) == 0 || bytes.Compare(r.HashedPK, sampleKey2) == 0 {
 			fmt.Println("ok")
@@ -143,6 +134,20 @@ func TestFMPKSimple(t *testing.T) {
 			fmt.Println("err")
 		}
 	}
+}
+
+func GenerateRandomReport(n int) (reports []backend.FMReport, hashKeys [][]byte) {
+	key := make([]byte, 16)
+	msg := make([]byte, 128)
+	for i := 0; i < n; i++ {
+		rand.Read(key)
+		hashKey := backend.Computehash(key)
+		hashKeys = append(hashKeys, hashKey)
+		rand.Read(msg)
+		report := backend.FMReport{HashedPK: hashKey, EncodedMsg: msg}
+		reports = append(reports, report)
+	}
+	return reports, hashKeys
 }
 
 func TestFMPKLong(t *testing.T) {
@@ -163,7 +168,10 @@ func TestFMPKLong(t *testing.T) {
 		}
 		fmReportJSON, err := json.Marshal(reports)
 		timeReportStart := time.Now()
-		_, err = httppost(fmt.Sprintf("https://%s/%s", endpoint, server.EndpointFMReport), fmReportJSON)
+		fmReportURL := fmt.Sprintf("https://%s/%s", endpoint, server.EndpointFMReport)
+		_, err = httppost(fmReportURL, fmReportJSON)
+		//fmt.Printf("\nPOST Report:\n curl -X POST \"%v\" -d '%v'\n", fmReportURL, string(fmReportJSON))
+
 		fmt.Printf("request %d time %v\n", reportNum, time.Since(timeReportStart))
 		if err != nil {
 			t.Fatalf("EndpointFMReport: %s", err)
@@ -189,7 +197,10 @@ func TestFMPKLong(t *testing.T) {
 		prefixHashedKey = append(prefixHashedKey, prefixSampleKey2...)
 
 		queryTimeStart := time.Now()
-		result, err := httppost(fmt.Sprintf("https://%s/%s/%d", endpoint, server.EndpointFMQuery, timeStart.Unix()), prefixHashedKey)
+		fmQueryUrl := fmt.Sprintf("https://%s/%s/%d", endpoint, server.EndpointFMQuery, timeStart.Unix())
+		fmt.Printf("\nPOST Query:\n curl -X POST \"%v\" --data-binary '%s'\n", fmQueryUrl, prefixHashedKey)
+
+		result, err := httppost(fmQueryUrl, prefixHashedKey)
 		fmt.Printf("query %d time %v\n", queryNum, time.Since(queryTimeStart))
 		if err != nil {
 			t.Fatalf("EndpointFMReport: %s", err)
@@ -202,7 +213,7 @@ func TestFMPKLong(t *testing.T) {
 		}
 		for _, r := range resultreport {
 			if bytes.Compare(r.HashedPK, sampleKey) == 0 || bytes.Compare(r.HashedPK, sampleKey2) == 0 {
-				//fmt.Println("ok")
+				fmt.Println("ok")
 			} else {
 				fmt.Println("err")
 			}
